29afe2b84642d6bf530390f02f03267c9df51783
Kenneth Bingham <w@qrk.us>
2024-02-19 16:48:56 -0500

    manage TLS with caddy

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 PROFILE=ziti
 docker compose --profile $PROFILE kill --remove-orphans
 docker compose --profile $PROFILE pull
-docker compose --profile $PROFILE up --force-recreate --detach
+docker compose --profile $PROFILE up --force-recreate --detach --build
 docker compose --profile $PROFILE logs --follow
