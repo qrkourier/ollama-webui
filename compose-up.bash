76b20aea8aecbb7908af312b9fcf6bae103b164c
Kenneth Bingham <w@qrk.us>
2024-03-09 15:41:51 -0500

    save

--- compose-up.bash
+++ compose-up.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 PROFILE=ziti
+export COMPOSE_FILE=docker-compose.yaml:docker-compose.override.yaml:docker-compose.ziti-router.yaml
 docker compose --profile $PROFILE kill --remove-orphans
 docker compose --profile $PROFILE pull
 docker compose --profile $PROFILE up --force-recreate --detach --build
