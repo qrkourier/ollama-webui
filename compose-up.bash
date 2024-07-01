2c7c5f90680cc8e1f6c721e9ebdb6464be481cb5
Kenneth Bingham <w@qrk.us>
2024-06-30 20:03:00 -0400

    add backup container

--- compose-up.bash
+++ compose-up.bash
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-#PROFILE=ziti
-#docker compose --profile $PROFILE kill --remove-orphans
-#docker compose --profile $PROFILE pull
-#docker compose --profile $PROFILE up --force-recreate --detach --build
-#docker compose --profile $PROFILE logs --follow
 
 docker compose kill
 docker compose down --remove-orphans
